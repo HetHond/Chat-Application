2b2d5dbf0ddc5e6a2a440a3da9882ac487a62805
Senna de Vos <hethond2020@outlook.com>
2024-04-23 16:40:10 +0200

    Added missing annotation

--- src/main/java/com/hethond/chatbackend/repositories/MessageRepository.java
+++ src/main/java/com/hethond/chatbackend/repositories/MessageRepository.java
@@ -2,9 +2,11 @@ package com.hethond.chatbackend.repositories;
 
 import com.hethond.chatbackend.entities.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findByChannelId(long channelId);
 }
