489ed45ef2fbddc48e256023eff54bff7be5514c
Senna de Vos <hethond2020@outlook.com>
2024-04-23 16:58:17 +0200

    Add new users to the welcome channel

--- src/main/java/com/hethond/chatbackend/services/UserService.java
+++ src/main/java/com/hethond/chatbackend/services/UserService.java
@@ -1,8 +1,10 @@
 package com.hethond.chatbackend.services;
 
 import com.hethond.chatbackend.ApiException;
+import com.hethond.chatbackend.entities.Channel;
 import com.hethond.chatbackend.entities.Role;
 import com.hethond.chatbackend.entities.User;
+import com.hethond.chatbackend.repositories.ChannelRepository;
 import com.hethond.chatbackend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
@@ -14,10 +16,13 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final ChannelService channelService;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+                       ChannelService channelService) {
         this.userRepository = userRepository;
+        this.channelService = channelService;
     }
 
     public List<User> findAllUsers() { return userRepository.findAll(); }
@@ -34,6 +39,8 @@ public class UserService {
 
     public User createUser(String username, String password) {
         User newUser = new User(username, BCrypt.hashpw(password, BCrypt.gensalt()), Role.USER);
+        Channel welcomeChannel = channelService.findChannelById(2);
+        welcomeChannel.addMember(newUser);
         return userRepository.save(newUser);
     }
 
